238c95294b597da67bf31182df5d2f2663d72002
Hutch Crowley <tothecoda@gmail.com>
2019-11-17 16:35:44 -0600

    Update answers component fix need

--- src/components/Answers/Answer.js
+++ src/components/Answers/Answer.js
@@ -1,52 +1,12 @@
-import React, { useState, useEffect } from "react"
-import api from "../utils/api"
-/*
+import React from "react"
 
-axios and api is already set up with an api function
-instead of importing axios you will just use
-
-api()
-    .get('/api/answers/:id')
-    .then('')
-    .catch('')
-
-
-
-
-step 1) Match question with answer
-step 2) Display answer in component below
-step 3) render Answer component below question in QuestionsList.js
-
-*/
-
-export default function Answer(props) {
-  // Here I am setting a slice of state for the id value that I will get from my API call, and another slice for the questionId, which I expect to recieve on props. (Awaiting prop from Whitney)
-  // I am then using the hook a third time to create a slice for the answer, which will come from somewhere. (AnswerList? QuestionList?)
-  const [identifier, setIdentifier] = useState("")
-  const [questionId, setQuestionId] = useState("")
-  const [answer, setAnswer] = useState("")
-
-  // Making my API call...
-  setQuestionId(props.questionid)
-  useEffect(() => {
-    api()
-      .get(`/api/answers`)
-      .then(res => {
-        setIdentifier(res.data.id)
-        if (identifier === questionId) setAnswer(res.data.answer)
-      })
-      .catch(err => {
-        console.log("Error, data was not returned from server", err)
-      })
-  }, [identifier, questionId])
-  //  Syncing state with my async API call...
-
+const Answer = props => {
   return (
-    <section className="answers">
-      <div>
-        {/* Displaying data here */}
-        <p>Here is the answer to your question: {answer}</p>
-      </div>
-    </section>
+    <div className="answer-wrapper">
+      <p>Answer: {props.answer}</p>
+      <p>Business Owner Username : {props.bus_owner_username}</p>
+    </div>
   )
 }
+
+export default Answer;
--- src/components/Questions/Question.js
+++ src/components/Questions/Question.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React from "react"
+import AnswerList from "../Answers/AnswerList"
 //import styled from 'styled-components'
 
 const Question = props => {
-    return (
-        <div className="question-bar" key={props.id}>
-            {/* <img
+  return (
+    <div className="question-bar" key={props.id}>
+      {/* <img
                 className="entrepreneur-avatar"
                 src={props.img}
                 alt="e-avatar"
                 /> */}
-            <h3>{props.e_username}</h3>
-            
-            <h1>{props.title}</h1>
-            <h2>Q: {props.question}</h2>
-            <p>Business: {props.business}</p>
+      <h3>{props.e_username}</h3>
 
+      <h1>{props.title}</h1>
+      <h2>Q: {props.question}</h2>
+      <p>Business: {props.business}</p>
 
-{/* 
+      {/*
 
             //Hutch the variable name you will need for the Answer List component is questionid={props.questionid} */}
-        </div>
-    )
+      <AnswerList question_id={props.questionid} />
+    </div>
+  )
 }
-export default Question;
+export default Question
