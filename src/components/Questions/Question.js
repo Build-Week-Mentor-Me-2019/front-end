f630fa62037079138b85b0d48cc9f9ab3b1bc151
Jesse-Tingle <jtingle0@gmail.com>
2019-11-17 18:35:50 -0500

    update to order on Question component

--- src/components/Questions/Question.js
+++ src/components/Questions/Question.js
@@ -5,20 +5,14 @@ import AnswerList from "../Answers/AnswerList"
 const Question = props => {
   return (
     <div className="question-bar" key={props.id}>
-      {/* <img
-                className="entrepreneur-avatar"
-                src={props.img}
-                alt="e-avatar"
-                /> */}
-      <h3>{props.e_username}</h3>
+      {/* <img className="entrepreneur-avatar"  src={props.img} alt="e-avatar" /> */}
 
       <h1>{props.title}</h1>
       <h2>Q: {props.question}</h2>
       <p>Business: {props.business}</p>
+      <h3>{props.e_username}</h3>
 
-      {/*
-
-            //Hutch the variable name you will need for the Answer List component is questionid={props.questionid} */}
+      {/* Hutch the variable name you will need for the Answer List component is questionid={props.questionid} */}
       <AnswerList question_id={props.questionid} />
     </div>
   )
