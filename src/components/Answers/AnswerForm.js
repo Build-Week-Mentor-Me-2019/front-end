2aba7870427ccc8e1206d1ac25826b397ec3f777
Hutch Crowley <tothecoda@gmail.com>
2019-11-17 16:54:32 -0600

    Update add AnswerForm component

--- src/components/Answers/AnswerForm.js
+++ src/components/Answers/AnswerForm.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { fetchPosts } from '../../redux/actions/postActions';
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { fetchPosts } from "../../redux/actions/postActions"
 
 /*
 Step 1) Create a form to submit a new answer to a question
 */
 
 class AnswerForm extends Component {
+  componentWillMount() {
+    this.props.fetchPosts()
+  }
 
-    componentWillMount() {
-        this.props.fetchPosts()
-    }
-     
-    render(){
-        return (
-        // <form>
-        //     <textarea name="answer"   />
-        //     <br />
-        //     <button type="submit">Submit</button>
-        // </form>
-
-        <div>
-
-        </div>
+  render() {
+    return (
+      <form className="answer-form">
+        <textarea name="answer" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     )
-    }
-    
+  }
 }
 
-export default connect(null, {fetchPosts})(AnswerForm);
+export default connect(null, { fetchPosts })(AnswerForm)
