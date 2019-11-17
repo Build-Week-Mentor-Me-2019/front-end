acd006a773ed269410fabd67d783c96f99e62c29
Hutch Crowley <tothecoda@gmail.com>
2019-11-17 15:49:15 -0600

    Update add state and props to component

--- src/components/Answers/Answer.js
+++ src/components/Answers/Answer.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import api from '../utils/api'
+import React, { useState, useEffect } from "react"
+import api from "../utils/api"
 /*
 
 axios and api is already set up with an api function
@@ -19,43 +19,34 @@ step 3) render Answer component below question in QuestionsList.js
 
 */
 
+export default function Answer(props) {
+  // Here I am setting a slice of state for the id value that I will get from my API call, and another slice for the questionId, which I expect to recieve on props. (Awaiting prop from Whitney)
+  // I am then using the hook a third time to create a slice for the answer, which will come from somewhere. (AnswerList? QuestionList?)
+  const [identifier, setIdentifier] = useState("")
+  const [questionId, setQuestionId] = useState("")
+  const [answer, setAnswer] = useState("")
 
+  // Making my API call...
+  setQuestionId(props.questionid)
+  useEffect(() => {
+    api()
+      .get(`/api/answers`)
+      .then(res => {
+        setIdentifier(res.data.id)
+        if (identifier === questionId) setAnswer(res.data.answer)
+      })
+      .catch(err => {
+        console.log("Error, data was not returned from server", err)
+      })
+  }, [identifier, questionId])
+  //  Syncing state with my async API call...
 
-export default function Answer (props)
-{
-
-    // Here I am setting a slice of state for the id value that I will get from my API call, and another slice for the questionId, which I expect to recieve on props. (Awaiting prop from Whitney)
-    // I am then using the hook a third time to create a slice for the answer, which will come from somewhere. (AnswerList? QuestionList?)
-    const [identifier, setIdentifier] = useState('');
-    const [questionId, setQuestionId] = useState('');
-    const [answer, setAnswer] = useState('');
-
-    // Making my API call...
-    setQuestionId(props.question_id)
-    useEffect(() =>
-    {
-        api()
-            .get(`/api/answers`)
-            .then(res =>
-            {
-                setIdentifier(res.data.id)
-                if (identifier === questionId)
-                    setAnswer(res.data.answer)
-            })
-            .catch(err =>
-            {
-                console.log('Error, data was not returned from server', err);
-            });
-    }, [identifier, questionId]);
-    //  Syncing state with my async API call...
-
-    return (
-        <section className='answers'>
-            <div>
-                {/* Displaying data here */}
-                <p>Here is the answer to your question: {answer}</p>
-            </div>
-
-        </section>
-    )
+  return (
+    <section className="answers">
+      <div>
+        {/* Displaying data here */}
+        <p>Here is the answer to your question: {answer}</p>
+      </div>
+    </section>
+  )
 }
